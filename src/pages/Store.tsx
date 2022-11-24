0bc5d59079a61ab3d7f320548fd5d46dba43c6ee
RamyALaa <ramy.aloocy@gmail.com>
2022-11-24 11:54:02 +0000

    navbar + store

---
+++ src/pages/Store.tsx
@@ -0,0 +1,14 @@
+import storeItems from "../data/items.json"
+import {Row, Col } from "react-bootstrap"
+const Store = () => {
+    return ( <>
+    <Row xs={1} md={2} lg={3} className="g-3">
+        {storeItems.map(item => (
+            <Col><StoreItem {...item}/></Col>
+        ))}
+        
+    </Row>
+    </> );
+}
+ 
+export default Store;
